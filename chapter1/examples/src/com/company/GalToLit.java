4870501081afd444b2f3d22ae393552ec890d534
thekeisi <htht@yahoo.com>
2019-02-26 23:16:32 +0400

    ex1 and ex2

--- chapter1/examples/src/com/company/GalToLit.java
+++ chapter1/examples/src/com/company/GalToLit.java
@@ -1,4 +1,9 @@
 package com.company;
 
 public class GalToLit {
+    public static void main(String[] args) {
+        double gal = 10;
+        double lit = gal * 3.7854;
+        System.out.println(gal+" гaллонов это "+lit+" литров");
+    }
 }
--- chapter1/examples/src/com/company/GalToLitTable.java
+++ chapter1/examples/src/com/company/GalToLitTable.java
@@ -1,4 +1,19 @@
 package com.company;
 
 public class GalToLitTable {
+    public static void main (String[] args){
+        double gal,lit;
+        for (int i=1;i<=100;i++){
+            if (i%10 != 0){
+                gal = i;
+                lit = gal * 3.7854;
+                System.out.println(gal+" gal = "+lit+" lit");
+            } else {
+                gal = i;
+                lit = gal * 3.7854;
+                System.out.println(gal+" gal = "+lit+" lit");
+                System.out.println();
+            }
+        }
+    }
 }
